626587240eb53d6710cbbcd671513d841bf9e31f
Joao <joaobaise@hotmail.com>
2022-05-03 00:40:14 +0100

    The system now knows how to handle proper inputs without errors. Documentation updated. Try and except added for IndexErrors.

--- MainMenu.py
+++ MainMenu.py
@@ -9,7 +9,7 @@ HEADERS = ["Items", "Price"]
 PRODUCTS = {
     "Items": ["0-Stereo System", "1-Leather Interior", "2-Global Positioning System(GPS)",
               "3-Standard - Free of Charge", "4-Modified", "5-Customized Detailing"],
-    "Prices": [30.50, 530.99, 301.90, 0, 370.50, 1257.99, 6]  # index 6 is the tax percentage
+    "Prices": [30.50, 530.99, 301.90, 0, 370.50, 1257.99]  # index 6 is the tax percentage
 }
 
 order = {"Items": [],
@@ -26,11 +26,11 @@ modify_products = Order.Input()
 #  Program Control Functions #
 ##############################
 
-
+# Control Flow
 def menuControl(var):
-    if var.isnumeric():
+    if var.isnumeric():  # Checking if it's a valid input
         return True
-    else:
+    else:       # if the condition doesn't satisfy, check for control flow options
         match var.lower():
             case 'reset' | 'restart':
                 reset()
@@ -59,7 +59,7 @@ def end():
 def additionalItems():
     while True:
         var = selection.getInput("Add an item?:(y/n) ")  # asks for input, returns str.
-        if menuControl(var):
+        if menuControl(var):  # Checks for Control Flow
             continue
         elif var.lower() == 'y':
             itemSelection()
@@ -70,11 +70,14 @@ def additionalItems():
 # Handles user input in correlation to the menu.
 def itemSelection():
     chosen_item = selection.getInput("Choose an Item: ")  # Asks for input, return str
-    if menuControl(chosen_item):
-        Order.Input.updateBill(order, PRODUCTS, chosen_item)  # Updates the selected item into the shopping list(order)
-        selection.returnBill(order, HEADERS)  # Return the order bill in form of a table
-    else:
-        print("Order Cleared ...")
+    try:
+        if menuControl(chosen_item):  # Checks for Control Flow.
+            Order.Input.updateBill(order, PRODUCTS, chosen_item)  # Updates item into the "shopping list" / order.
+            selection.returnBill(order, HEADERS)  # Return the order bill in form of a table
+        else:
+            print("Order Cleared ...")
+    except IndexError:
+        print("You can only choose from 0 to 5 items")
 
 
 # Choose items to be added to the bill.
@@ -92,45 +95,47 @@ def selectItem():
 def play():
     print("You can type clear, restart or exit anytime in the program.")
     while True:
-        selection.clearBill(order)
+        selection.clearBill(order)  # Clears the "shopping basket" before starting a new order.
+        #
         # First input, Price of the car
         car_input = car_price.getInput("Initial car sale price: ")
-        menuControl(car_input)
-        checked_price = car_price.checkValidity(car_input, False)
+        menuControl(car_input)  # Checks for Control Flow.
+        checked_price = car_price.checkValidity(car_input, False)  # Check for valid input
         price = checked_price
-        #  price = car_price.checkValidity(menuControl(car_price.getInput("Initial car sale price: ")), False)
 
         if int(price) < 1:  # ONLY allows the program to proceed if price is a number
             continue
         else:
-            # Second input, allowance if any
+            # Second input
             allowance_input = allowance.getInput("Trade-in allowance: ")
             menuControl(allowance_input)  # Check for Control Flow
-            checked_allowance = allowance.checkValidity(allowance_input, True)
+            checked_allowance = allowance.checkValidity(allowance_input, True)  # Check for valid input
             trade_in_allowance = checked_allowance
 
-            # trade_in_allowance = allowance.checkValidity(allowance.getInput("Trade-in allowance: "), True)
+            # After both inputs are checked, proceed with the program.
             # Proceed to menu, select items and calculate order
-            print("Basic Car Price Entered:", int(price), int(trade_in_allowance), end='\n')
+            print("Basic Car Price Entered: ", int(price),
+                  "\nTrade-in Allowance: ", int(trade_in_allowance), end='\n')  # Display inputs
             print("-" * 46)
-            promptProducts()
-            selectItem()
-            calculate(price, trade_in_allowance)
+            promptProducts()  # display database
+            selectItem()  # handles user choices
+            calculate(price, trade_in_allowance)  # Calculations
             break
 
 
 # Handles all calculations within the system
 def calculate(price, trade_in_allowance):
-    # calculates the subtotal of inputs and the order+
-    # formula is amount divided by 100 multiplied by percentage
+    # Calculates the subtotal of inputs and the order/"shopping basket"
+    # formula of % : is amount divided by 100 multiplied by percentage
 
     # Sum of all the items in order, plus the car price
+    # order dictionary has to be converted into a list because of sum().
     lst = list(order['Prices'])
     subtotal = sum(lst) + int(price)
     print("Subtotal: ", "£{:,.2f}".format(subtotal))
 
     # Calculate the sales tax based on the subtotal
-    sales_tax = (subtotal / 100) * int(PRODUCTS['Prices'][-1])
+    sales_tax = (subtotal / 100) * 6
     print("Sales Tax: ", "£{:,.2f}".format(sales_tax))
 
     # Calculate the overall price after allowance.
@@ -144,6 +149,7 @@ def calculate(price, trade_in_allowance):
 
 # Displays Additional Accessories Table.
 def promptProducts():
+    # Prints database
     selection.returnBill(PRODUCTS, HEADERS)
 
 
@@ -151,20 +157,22 @@ def promptProducts():
 def changePrice():
     while True:
         var = modify_products.getInput("Would you like to change the price of a item?:(y/n) ")
-        if menuControl(var):
+        if menuControl(var):  # checks Control Flow.
             continue
         elif var.lower() == "y":
             try:
                 modify_item = modify_products.getInput("Which item would you like to change?:")
-                print(modify_item)
+                item = int(modify_item)
+                print("Selected Item: ", PRODUCTS['Items'][item])
                 new_value = modify_products.getInput("What would you like to change the price of the item to?: ")
-                print(new_value)
+                print("New value of", PRODUCTS['Items'][item], ": £", new_value)
+                # updates the PRODUCTS dictionary with the parsed item to change and its new value.
                 modify_products.updateProducts(PRODUCTS, int(modify_item), int(new_value))
-                print("New prices updated to additional accessories: ")
+                print("New prices updated to additional accessories database... ")
 
                 break
-            except ValueError:
-                print("Integers only")
+            except IndexError:
+                print("You can only choose from 0 to 5 items")
         elif var.lower() == 'n':
             break
     return False
